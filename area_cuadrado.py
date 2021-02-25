d4b00c03d629d4afa01f51435f2ccff124d0b134
tecfil3d@gmail.com <D1m0nb1j0ux>
2021-02-25 22:14:01 +0100

    Prueba de creación y subida

---
+++ area_cuadrado.py
@@ -0,0 +1,16 @@
+# Hallar el área de un cuadrado
+# Codigo:
+
+"""
+b = base
+a = altura
+area = base * altura
+"""
+b = float(input("introduzca la base: "))
+
+a = float(input("introduzca la altura: "))
+
+area = b * a
+
+print("El area del cuadrado es: ", area)
+
