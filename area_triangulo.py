7d7b1351d2c1691ad2bfcd88294571e50a8393b1
Diego <Tecfil3D@gmail.com>
2021-02-25 14:26:32 +0100

    Area de un triangulo

---
+++ area_triangulo.py
@@ -0,0 +1,14 @@
+# Hallar el área de un triangulo
+# Datos:
+
+"""
+ B = base
+ A = altura
+ Area = (base * altura) / 2
+
+"""
+B = float(input("ingrese la base: "))
+A = float(input("ingrese la altura: "))
+Area = (B * A)/2
+print("El área del triangulo resultante es:", Area)
+
