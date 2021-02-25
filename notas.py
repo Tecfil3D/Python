6f287e9d27b427ae19ee76d905f59703134e0861
Diego <Tecfil3D@gmail.com>
2021-02-25 14:46:09 +0100

    promedio de notas

---
+++ notas.py
@@ -0,0 +1,20 @@
+# Hallar el promedio de tres notas
+# Datos:
+
+"""
+n1 = nota 1
+n2 = nota 3
+n3 = nota 3
+
+p = promedio
+
+promedio = (nota 1 + nota 2 + nota 3)/ 3
+
+"""
+n1 = float(input("Introduce nota 1: "))
+n2 = float(input("Introduce nota 2: "))
+n3 = float(input("Introduce nota 3: "))
+
+p = float(n1 + n2 + n3) / 3
+
+print("El promedio de nota final es:", p,"puntos")
