ff44f0957767cf345ce0ad8754063f15209b2a41
Diego <Tecfil3D@gmail.com>
2021-03-01 16:06:13 +0100

    Primer commit

---
+++ variables.py
@@ -0,0 +1,22 @@
+# Variables
+
+string = 'Hola esto es un string.'
+print(string)
+
+string = "Cambiando el valor de string."
+print(string)
+
+a, b, c = 1, 4, 7
+print('El valor de las variables de a, b y c son:',a, b ,c)
+
+# 'del' nos permite eliminar variables
+
+del a, b, c
+
+string = 'Cadena de texto'
+integer = 12
+float = 3.14
+
+concatenar = string,str(integer), str(float)
+print(concatenar)
+
