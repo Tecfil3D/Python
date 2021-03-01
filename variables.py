a073ae53089a9887b77ed53b4e0a7c1cfb8987be
tecfil3d@gmail.com <D1m0nb1j0ux>
2021-03-01 20:14:14 +0100

    Clase de variables

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-# Variables
+# Variables.
 
 string = 'Hola esto es un string.'
 print(string)
@@ -7,9 +7,9 @@ string = "Cambiando el valor de string."
 print(string)
 
 a, b, c = 1, 4, 7
-print('El valor de las variables de a, b y c son:',a, b ,c)
+print('El valor de las variables de a, b y c son:', a, b, c)
 
-# 'del' nos permite eliminar variables
+# 'del' nos permite eliminar variables.
 
 del a, b, c
 
@@ -17,6 +17,18 @@ string = 'Cadena de texto'
 integer = 12
 float = 3.14
 
-concatenar = string,str(integer), str(float)
+# Con str convertimos los números int en string para poderlos concatenar,
+# concatenamos las comillas vacías para generar un espacio entre los datos.
+
+concatenar = string + ' ' + str(integer) + ' ' + str(float)
 print(concatenar)
 
+# Sumar un string + int
+
+string = '4'
+integer = 1
+
+# Convertimos el dato string en entero
+
+total = int(string) + integer
+print('El total es: ', total)
