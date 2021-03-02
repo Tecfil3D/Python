a1d31d3fb45f69962368914e56ba0a5210ada252
tecfil3d@gmail.com <D1m0nb1j0ux>
2021-03-02 23:27:39 +0100

    Clase de operadores

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -1,10 +1,54 @@
 # Operadores aritmeticos
 
-# Suma
+# Suma +
 
 numero1 = 10
-numero2 = 5
+numero2 = 3
 
 total_suma = numero1 + numero2
 print('la suma entre', numero1, 'y', numero2, 'es igual a', total_suma)
+
+# Resta -
+
+total_resta = numero1 - numero2
+print('la resta entre', numero1, 'y', numero2, 'es igual a', total_resta)
+
+# Multiplicación *
+
+total_multiplicacion = numero1 * numero2
+print('la multipicación entre', numero1, 'y', numero2, 'es igual a', total_multiplicacion)
+
+# Cociente de una división /
+
+total_division = numero1 / numero2
+print('el cociente de dividir', numero1, 'entre', numero2, 'es igual a', total_division)
+
+# Cociente de una division //
+
+total_cociente = numero1 // numero2
+print('el cociente de dividir', numero1, 'entre', numero2, 'es igual a', total_cociente)
+
+# Resto de una division %
+
+total_resto = numero1 % numero2
+print('el resto de dividir', numero1, 'entre', numero2, 'es igual a', total_resto)
+
+# Potencias ** (** significa elevado a...)
+
+total_potencia = numero1 ** numero2
+print('el resultado de', numero1, 'elevado a', numero2, 'es igual a', total_potencia)
+
+# Asignación acumulada
+variable = 10
+variable += 5
+variable += 2
+print('el valor de la variable es igual a', variable)
+variable -= 2
+print('el valor de la variable ahora es igual a', variable)
+
+
+
+
+
+
 
