8a667c274da6b8a216cf36783f9ee6ce4da6e502
tecfil3d@gmail.com <D1m0nb1j0ux>
2021-03-03 19:17:41 +0100

    Clase de diccionarios

--- diccionario.py
+++ diccionario.py
@@ -1,4 +1,16 @@
 # Diccionarios
 
+diccionario = {'1': 'Marcos', '2': 'Luis', '3': 'Oscar', 4: 'Miguel'}    # se pueden añadir infinitamente mas datos
+print(diccionario['3'])    # imprime la información alojada en la clave que corresponde a ['2']
+print(diccionario[4])
+# Imprime las claves del diccionario
 
+print('Las claves del diccionario son:', diccionario.keys())
 
+# Imprime los valores del diccionario
+
+print('Los valores del diccionario son:', diccionario.values())
+
+# Imprime el diccionario completo
+
+print('El diccionario completo:', diccionario)
