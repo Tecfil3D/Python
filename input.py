a8a08e45dbf7d33a42c234548f79b131ef685b84
tecfil3d@gmail.com <D1m0nb1j0ux>
2021-03-05 23:12:06 +0100

    Clase input

--- input.py
+++ input.py
@@ -2,8 +2,9 @@
 
 print('Programa para sumar dos números ****')
 
-numero1 = float(input('introduzca el primer número: '))
-numero2 = float(input('introduzca el segundo número: '))
+# convertimos a flotante el valor input
 
-total = (numero1 + numero2)
+numero1 = float(input('introduzca el primer número: '))    # en numero1 guardamos el valor que introduzca el usuario
+numero2 = float(input('introduzca el segundo número: '))    # en numero guardamos el valor que introduzca el usuario
+total = (numero1 + numero2)    # Sumamos las dos variables
 print('La suma de', numero1, 'y', numero2, 'es igual a:', total)
