36d3fbea7e4db7b0b8f74e2ecd2922a3e7820ae8
tecfil3d@gmail.com <D1m0nb1j0ux>
2021-03-05 23:08:10 +0100

    Clase input

---
+++ input.py
@@ -0,0 +1,9 @@
+# input
+
+print('Programa para sumar dos números ****')
+
+numero1 = float(input('introduzca el primer número: '))
+numero2 = float(input('introduzca el segundo número: '))
+
+total = (numero1 + numero2)
+print('La suma de', numero1, 'y', numero2, 'es igual a:', total)
